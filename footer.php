58ec76fce28007870987fa20f2f1995f4ec1a8b7
Paul Schaefer <yeppaul2@gmail.com>
2016-04-04 21:28:27 -0400

    improved footer responsiveness

--- footer.php
+++ footer.php
@@ -6,9 +6,9 @@
 </div>
 <div class='grid__col--1-of-3 grid__col'>
 Share with<br>
-<a href='https://facebook.com/sharer/sharer.php?u=<?php echo urlencode(site_url()); ?>' target='_blank' class='icon js-social-share facebook ion-social-facebook'></a>
-<a href='https://twitter.com/intent/tweet/?text=Paul%20Schaefer,%20digital%20artist%20and%20web%20developer&via=pschfr&url=<?php echo urlencode(site_url()); ?>' target='_blank' class='icon js-social-share twitter ion-social-twitter'></a>
-<a href='mailto:?to=%20&subject=Look%20at%20this%20rad%20website!&body=<?php echo urlencode(site_url()); ?>' class='icon email ion-ios-email'></a><br>
+<a href='https://facebook.com/sharer/sharer.php?u=<?php echo urlencode(site_url()); ?>' target='_blank' class='icon js-social-share ion-social-facebook'></a>
+<a href='https://twitter.com/intent/tweet/?text=Paul%20Schaefer,%20digital%20artist%20and%20web%20developer&via=pschfr&url=<?php echo urlencode(site_url()); ?>' target='_blank' class='icon js-social-share ion-social-twitter'></a>
+<a href='mailto:?to=%20&subject=Look%20at%20this%20rad%20website!&body=<?php echo urlencode(site_url()); ?>' class='icon ion-ios-email'></a><br>
 </div>
 <div class='grid__col--1-of-3 grid__col'>
 Beam me up<br>
