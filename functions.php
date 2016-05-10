ad65dd719b0dd156de4ed0d714519979dc5f4e42
Paul <yeppaul2@gmail.com>
2016-05-09 21:57:10 -0400

    removed extra code

--- functions.php
+++ functions.php
@@ -190,33 +190,22 @@ add_theme_support('post-thumbnails');
 // Adds custom post types to Right Now widget in admin
 function vm_right_now_content_table_end() {
     $args = array(
-        'public' => true ,
+        'public'   => true ,
         '_builtin' => false
     );
-    $output = 'object';
+    $output   = 'object';
     $operator = 'and';
-    $post_types = get_post_types( $args , $output , $operator );
-    foreach( $post_types as $post_type ) {
-        $num_posts = wp_count_posts( $post_type->name );
-        $num = number_format_i18n( $num_posts->publish );
-        $text = _n( $post_type->labels->name, $post_type->labels->name , intval( $num_posts->publish ) );
-        if ( current_user_can( 'edit_posts' ) ) {
+    $post_types = get_post_types($args, $output, $operator);
+    foreach($post_types as $post_type) {
+        $num_posts = wp_count_posts($post_type->name);
+        $num = number_format_i18n($num_posts->publish);
+        $text = _n($post_type->labels->name, $post_type->labels->name , intval($num_posts->publish));
+        if (current_user_can('edit_posts'))
             $cpt_name = $post_type->name;
-        }
-        echo '<li class="post-count"><tr><a href="edit.php?post_type='.$cpt_name.'"><td class="first b b-' . $post_type->name . '"></td>' . $num . '&nbsp;<td class="t ' . $post_type->name . '">' . $text . '</td></a></tr></li>';
-    }
-    $taxonomies = get_taxonomies( $args , $output , $operator );
-    foreach( $taxonomies as $taxonomy ) {
-        $num_terms  = wp_count_terms( $taxonomy->name );
-        $num = number_format_i18n( $num_terms );
-        $text = _n( $taxonomy->labels->name, $taxonomy->labels->name , intval( $num_terms ));
-        if ( current_user_can( 'manage_categories' ) ) {
-            $cpt_tax = $taxonomy->name;
-        }
-        echo '<li class="post-count"><tr><a href="edit-tags.php?taxonomy='.$cpt_tax.'"><td class="first b b-' . $taxonomy->name . '"></td>' . $num . '&nbsp;<td class="t ' . $taxonomy->name . '">' . $text . '</td></a></tr></li>';
+        echo '<li class="post-count"><tr><a href="edit.php?post_type=' . $cpt_name . '"><td class="first b b-' . $post_type->name . '"></td>' . $num . '&nbsp;<td class="t ' . $post_type->name . '">' . $text . '</td></a></tr></li>';
     }
 }
-add_action( 'dashboard_glance_items' , 'vm_right_now_content_table_end' );
+add_action('dashboard_glance_items' , 'vm_right_now_content_table_end');
 
 // Custom WordPress Admin Footer
 add_filter('admin_footer_text', 'remove_footer_admin');
